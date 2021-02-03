77f1383a98c251d9977f55514c89edfbbfe6e744
AironDev <airondev@gmail.com>
2021-02-03 20:35:58 +0100

    jetstream auth scaffold

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,22 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique()->nullable()->index();
+            $table->string('display_name')->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+             $table->string('api_token')->nullable();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
+            $table->text('cover_photo_path')->nullable();
+            $table->string('presence')->nullable()->default('online');
+            $table->string('status')->nullable()->default('active');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
