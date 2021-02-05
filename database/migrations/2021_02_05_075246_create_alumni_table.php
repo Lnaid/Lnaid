c62fa221136cc3701d06cc46d6f0117bd5d8cfe8
AironDev <airondev@gmail.com>
2021-02-05 15:15:53 +0100

    database migrations created

--- database/migrations/2021_02_05_075246_create_alumni_table.php
+++ database/migrations/2021_02_05_075246_create_alumni_table.php
@@ -14,8 +14,28 @@ class CreateAlumniTable extends Migration
     public function up()
     {
         Schema::create('alumni', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('school_id')->nullable();
+            $table->string('prefered_pronoun')->nullable();
+            $table->text('about')->nullable(); //rich text here
+
+            $table->string('course')->nullable();
+            $table->string('department')->nullable();
+            $table->string('falculty')->nullable();
+            $table->string('graduation_year')->nullable();
+
+            $table->string('phone_1')->nullable();
+            $table->string('phone_2')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('facebook')->nullable();
+
+
             $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('school_id')->references('id')->on('schools')->onDelete('cascade');
         });
     }
 
