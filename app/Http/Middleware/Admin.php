29d41ed1c995367266fe80e4e4c091d2e054c647
AironDev <airondev@gmail.com>
2021-02-09 17:11:42 +0100

    rbac middleware init

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+use Silber\Bouncer\BouncerFacade as Bouncer;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // if ($request->user()->role !== 'admin') {
+        //     return redirect('/');
+        // }
+        // return $next($request);
+        $user = $request->user();
+
+        if(Bouncer::is($user)->a('superadmin', 'admin', 'moderator')){
+            return $next($request);
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+    }
+}
