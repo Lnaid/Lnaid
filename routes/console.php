9ba3a53179be0dda1865d52965ae8db447641a42
AironDev <airondev@gmail.com>
2021-02-09 17:20:59 +0100

    rbac console command

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Silber\Bouncer\BouncerFacade as Bouncer;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,53 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->describe('Display an inspiring quote');
+
+
+// Artisan::command('rbac:assign {--role= : Role to assign}{--userID= : The ID or Email of the user }', function ($role, $userID) {
+// 	$user = \App\User::where('id', $userID)->first();
+// 	//$this->info($user->name);
+// 	$this->info("Assigning {$role} to {$user->name}!");
+// })->describe('Display an inspiring quote');
+
+
+Artisan::command('rbac:setup {--superadmin}', function () {
+
+	if( $this->option('superadmin') ){
+		$userID = $this->ask('Enter user id or email?');
+		$user = User::where('id', $userID)->orWhere('email', $userID)->first();
+		$role = Bouncer::role()->where('name', 'superadmin')->first();
+
+		if( $user == null ){
+			return $this->info('User not found');
+		}
+		$user = $user->first();
+		$this->info("{$user->name} will be assigned superadmin role ...");
+		if ( $this->confirm('Do you wish to continue?') ) {
+			Bouncer::assign($role)->to($user);
+   			return $this->info('superadmin role assigned successfully');
+		}	
+	}else{
+
+		$roleName = $this->ask('Enter role name?');
+		$role = Bouncer::role()->where('name', $roleName)->first();
+		if( $role == null ){
+			return $this->info('Role not found');
+		}
+
+		$userID = $this->ask('Enter user id or email?');
+		$user = User::where('id', $userID)->orWhere('email', $userID)->first();
+		if($user == null){
+			return $this->info('User not found');
+		}
+		
+		$this->info("{$user->name} will be assigned {$role->name} role on culconnect");
+
+		if ($this->confirm('Do you wish to continue?')){
+			Bouncer::assign($role)->to($user);
+	   		return $this->info("{$role->name} role assigned to {$user->name} successfully");
+		}
+
+	}
+
+})->describe('Allow RBAC to be setup via console');
