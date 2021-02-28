2693290e4e67d0dd27079a0a8dcdd06021f5ee95
AironDev <airondev@gmail.com>
2021-02-28 02:33:14 +0100

    paytack init wip

---
+++ app/Http/Controllers/PaystackSubccountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaystackSubccount;
+use Illuminate\Http\Request;
+
+class PaystackSubccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PaystackSubccount  $paystackSubccount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PaystackSubccount $paystackSubccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PaystackSubccount  $paystackSubccount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PaystackSubccount $paystackSubccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PaystackSubccount  $paystackSubccount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PaystackSubccount $paystackSubccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PaystackSubccount  $paystackSubccount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PaystackSubccount $paystackSubccount)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_02_28_010427_create_paystack_subccounts_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaystackSubccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paystack_subccounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('account_id')->comment('from paystack');
+
+
+            $table->string('integration')->nullable();
+            $table->string('domain')->nullable();
+            $table->string('subaccount_code')->nullable();
+            $table->string('business_name')->nullable()->comment('this should probably be students account');
+            $table->string('description')->nullable();
+            $table->string('primary_contact_name')->nullable();
+            $table->string('primary_contact_email')->nullable();
+            $table->string('primary_contact_phone')->nullable();
+            $table->string('percentage_charge')->nullable();
+            $table->boolean('is_verified')->nullable();
+            $table->string('settlement_bank')->nullable();
+            $table->string('account_number')->nullable();
+            $table->string('settlement_schedule')->nullable();
+            $table->boolean('active')->nullable();
+            $table->boolean('migrate')->nullable();
+            $table->string('settlement_bank')->nullable();
+            $table->timestamps();
+
+            $table->softDeletes();
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('paystack_subaccount_id')->references('id')->on('paystack_subaccounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paystack_subccounts');
+    }
+}
